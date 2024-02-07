f42690beecf4da3584587ee3c571fc70362e608a
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2024-02-07 17:50:22 -0300

    finished 2 part

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreateController
 
 class TagCreatorView:
     '''
@@ -7,9 +8,11 @@ class TagCreatorView:
     '''
     
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreateController()
+        
         body = http_request.body
         product_code = body["product_code"]
         
-        print('test')
+        formatted_response = tag_creator_controller.create(product_code)
         
-        return HttpResponse(status_code=200, body ={"hello": "world"})
+        return HttpResponse(status_code=200, body =formatted_response)
